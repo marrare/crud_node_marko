9723e116b898ae44de8458976531bb55077fd9b5
Rivaldo <rivaldo.alvesb36@gmail.com>
2022-05-05 07:04:18 -0300

    feat :sparkles: run project with docker-compose

--- app/config/dbConnection.js
+++ app/config/dbConnection.js
@@ -1,10 +1,13 @@
 var mysql = require('mysql2');
+require('dotenv').config()
+
 
 var connMySQL = function(){
 	return mysql.createConnection({
-		host : 'localhost',
-		user : 'rivaldo',
-		password : 'MKO)nji9',
+		host : process.env.DB_IP,
+		port: process.env.DB_PORT,
+		user : process.env.DB_USER,
+		password : process.env.DB_PASS,
 		database : 'crud_node',
         charset: "utf8_general_ci"
 	});
